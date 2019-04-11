6972b6327c778b6c81282c5555bec7073b7c7c4e
Michail Bondarenko <mcmikius15@gmail.com>
2019-04-11 11:11:23 +0300

    init SearchCentreWorld

--- MyRobot/SwiftRobotControlCenter.swift
+++ MyRobot/SwiftRobotControlCenter.swift
@@ -9,10 +9,12 @@
 import UIKit
 //  All robot commands can be founded in GameViewController.h
 class SwiftRobotControlCenter: RobotControlCenter {
-    
+    /*
+     На уровнях L4H/L55H/L555H/L666H - найти центр мира (симуляторы iPad Air/Pro)
+     */
     //  Level name setup
     override func viewDidLoad() {
-        levelName = "L0C" //  Level name
+        levelName = "L4H" //  Level name
         
         super.viewDidLoad()
     }
@@ -20,8 +22,65 @@ class SwiftRobotControlCenter: RobotControlCenter {
     override func viewDidAppear(_ animated: Bool) {
         
         super.viewDidAppear(animated)
-        
-        
+        getCenterOfArea()
+    }
+    func getCenterOfArea() {
+        searchCenter()
+        faceToDown()
+        if frontIsClear {
+            move()
+        }
+        searchCenter()
+    }
+    func searchCenter(){
+        corners()
+        searchLineCenter()
+    }
+    func faceToDown() {
+        if facingRight {
+            turnRight()
+        } else {
+            turnLeft()
+        }
+    }
+    func turnLeft() {
+        for _ in 0..<3 {
+            turnRight()
+        }
+    }
+    func corners() {
+        put()
+        while frontIsClear {
+            move()
+        }
+        put()
+        turnArround()
+    }
+    func turnArround() {
+        turnRight()
+        turnRight()
+        if frontIsClear {
+            move()
+        }
+    }
+    func searchLineCenter() {
+        while noCandyPresent {
+            
+            while noCandyPresent {
+                if frontIsClear {
+                    move()
+                }
+            }
+            turnArround()
+            put()
+            move()
+        }
+        if facingDown {
+            turnArround()
+        }
+        if facingRight {
+            turnArround()
+        }
     }
     
 }
