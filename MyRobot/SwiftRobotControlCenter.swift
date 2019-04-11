4aaaa869eefc31e77c3d3b6b2f2e2c1f28fce5b3
Michail Bondarenko <mcmikius15@gmail.com>
2019-04-11 10:54:21 +0300

    initial commit to brand double candy done

--- MyRobot/SwiftRobotControlCenter.swift
+++ MyRobot/SwiftRobotControlCenter.swift
@@ -9,10 +9,12 @@
 import UIKit
 //  All robot commands can be founded in GameViewController.h
 class SwiftRobotControlCenter: RobotControlCenter {
-    
+    /*
+     L1C/L11C - удвоить конфеты на том же месте (Dir)L1C/L11C - выглядят одинаково
+     */
     //  Level name setup
     override func viewDidLoad() {
-        levelName = "L0C" //  Level name
+        levelName = "L11C" //  Level name
         
         super.viewDidLoad()
     }
@@ -21,7 +23,51 @@ class SwiftRobotControlCenter: RobotControlCenter {
         
         super.viewDidAppear(animated)
         
-        
+        move()
+        doubleCandy()
+        move()
+        backCandyToStart()
+        comeBackToStart()
     }
     
+    
+    func turnLeft() {
+        for _ in 0..<3 {
+            turnRight()
+        }
+    }
+    func arround() {
+        for _ in 0..<2 {
+            turnRight()
+        }
+    }
+    func comeBackToStart() {
+        arround()
+        move()
+        move()
+        arround()
+    }
+    func doubleCandy() {
+        while candyPresent {
+            pick()
+            move()
+            put()
+            put()
+            comeBackToStart()
+            if candyInBag {
+                move()
+            }
+        }
+    }
+    func backCandyToStart() {
+        while candyPresent {
+            pick()
+            arround()
+            move()
+            arround()
+            put()
+            move()
+        }
+    }
 }
+
